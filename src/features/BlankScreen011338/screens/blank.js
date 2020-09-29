b6fe57f72591b6eac439df5a6a58ab9438153c67
Crowdbotics <team@crowdbotics.com>
2020-09-29 09:15:37 +0000

    Update components

--- src/features/BlankScreen011338/screens/blank.js
+++ src/features/BlankScreen011338/screens/blank.js
@@ -38,6 +38,9 @@ export class __Blank extends React.Component {
         }}
         style={this.props.themedStyle.Image_5}
       />
+      <Text style={this.props.themedStyle.Text_7}>
+        some sample text to show
+      </Text>
     </View>
   )
 }
@@ -114,6 +117,36 @@ _Blank = withStyles(__Blank, theme => ({
     borderTopWidth: 0,
     borderBottomWidth: 0,
     borderRadius: 0
+  },
+  Text_7: {
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "left",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
   }
 }))
 
