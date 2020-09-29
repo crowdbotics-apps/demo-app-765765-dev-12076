f399fb80712c2b2ff52b5b33b498c652a0baa156
Crowdbotics <team@crowdbotics.com>
2020-09-29 09:01:39 +0000

    Update components

--- src/features/BlankScreen011338/screens/blank.js
+++ src/features/BlankScreen011338/screens/blank.js
@@ -19,7 +19,7 @@ export class __Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
-  state = { Datepicker_4: new Date("09/29/2020") }
+  state = { Datepicker_4: new Date("09/29/2020"), Toggle_8: true }
   render = () => (
     <View style={this.props.themedStyle.View_1}>
       <Text style={this.props.themedStyle.Text_2}>Sample text content</Text>
@@ -59,6 +59,15 @@ export class __Blank extends React.Component {
             "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/download_S7Ixuoo.jpeg"
         }}
         style={this.props.themedStyle.Image_6}
+      />
+      <Toggle
+        text="switch ON/OFF"
+        activeColor="#409EFF"
+        inactiveColor="#C0CCDA"
+        disabled={false}
+        style={this.props.themedStyle.Toggle_8}
+        checked={this.state.Toggle_8}
+        onChange={nextChecked => this.setState({ Toggle_8: nextChecked })}
       />
     </View>
   )
@@ -226,6 +235,31 @@ _Blank = withStyles(__Blank, theme => ({
     borderTopWidth: 0,
     borderBottomWidth: 0,
     borderRadius: 0
+  },
+  Toggle_8: {
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "left",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
   }
 }))
 
